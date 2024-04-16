ee2d546fb8ba90eb814e875fabc08d9fcc2ca21e
Yogesh Samtani <yogeshsamtani@gmail.com>
2024-04-16 23:01:30 +0200

    crear node

---
+++ Back/server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import { PORT } from './src/config/env.js';
+import { route404 } from './src/controllers/errors/route404.js';
+
+
+
+
+const app = express();
+
+app.use(cors())
+
+// middleware 404
+app.use('*', route404);
+
+app.listen(PORT || 3000, () => {
+  console.log(`Escuchando el puerto ${PORT || 3000}...`);
+});
