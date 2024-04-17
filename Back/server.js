4aeb894e021eea1e9d8df0288d0d9a83effc1818
Mariano Carnevale <marianocarne900@gmail.com>
2024-04-17 18:43:22 +0200

    Creacion manejo de errores

--- Back/server.js
+++ Back/server.js
@@ -6,10 +6,25 @@ import { route404 } from './src/controllers/errors/route404.js';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
-// middleware 404
-app.use('*', route404);
+// Middleware de 404
+app.use((req, res) => {
+  res.status(404).send({
+    status: 'error',
+    message: 'Not found',
+  });
+});
+
+// Middleware de gestión de errores
+app.use((error, req, res, next) => {
+  console.error(error);
+
+  res.status(error.httpStatus || 500).send({
+    status: 'error',
+    message: error.message,
+  });
+});
 
 app.listen(PORT || 3000, () => {
   console.log(`Escuchando el puerto ${PORT || 3000}...`);
