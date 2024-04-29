754ce030cdc9ef3fcb8b094a934c649a3afbffe9
Mariano Carnevale <marianocarne900@gmail.com>
2024-04-29 21:41:26 +0200

    Instalacion de Vite, dependencias y carpetas

---
+++ Front/src/App.jsx
@@ -0,0 +1,22 @@
+// import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header />
+      <main>
+        {/* <Routes> */}
+        {/* <Route element={<PublicRoute />}>
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/register" element={<Register />} />
+          </Route> */}
+        {/* </Routes> */}
+      </main>
+      <footer />
+    </>
+  );
+}
+
+export default App;
