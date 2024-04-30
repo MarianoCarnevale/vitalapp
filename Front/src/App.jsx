cff9443e4d002a9b5f42846c5aceff82d8fd6a90
Mariano Carnevale <marianocarne900@gmail.com>
2024-04-30 21:54:58 +0200

    Registro de paciente OK

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,7 +1,26 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
