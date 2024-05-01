ed38467a5190239e1fd7efbf833eb76c35a620e0
Mariano Carnevale <marianocarne900@gmail.com>
2024-05-01 20:04:29 +0200

    agregado setState para doctor, y toast, error en validar joi Doctor

--- Front/src/pages/Register.jsx
+++ Front/src/pages/Register.jsx
@@ -2,34 +2,68 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import { joiResolver } from "@hookform/resolvers/joi";
 import { registerSchema } from "../schemas/registerSchema";
-//import { useState } from "react";
+// import { registerDoctorSchema } from "../schemas/registerDoctorSchema";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState, useEffect } from "react";
+
 const Register = () => {
+  const [role, setRole] = useState("Selecciona un rol");
+  const [discipline, setDiscipline] = useState([]);
+
+  // Combina tus esquemas
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     watch,
+    reset,
   } = useForm({
     mode: "onTouched",
     resolver: joiResolver(registerSchema),
+    // resolver: joiResolver(
+    //   role === "doctor" ? registerDoctorSchema : registerSchema
+    // ),
   });
 
+  useEffect(() => {
+    const fetchDiscipline = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/disciplines");
+        const disciplinesArray = Object.values(response.data.data.disciplines); // Cambia esto
+        setDiscipline(disciplinesArray);
+        console.log(disciplinesArray);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDiscipline();
+  }, []);
+
   const onSubmit = handleSubmit(async (data) => {
-    console.log(data); // Imprime los datos del formulario
     delete data.confirmarpassword;
     try {
       const response = await axios.post(
         "http://localhost:3000/users/register",
         data
       );
-      console.log(response.data);
+
+      if (response.data.status === "ok") {
+        toast.success("Usuario registrado correctamente");
+        reset();
+        return;
+      }
     } catch (error) {
-      console.error(error);
+      toast.error(error.response.data.message);
+      console.log(error.response.data.message);
     }
   });
 
   return (
     <div className="w-3/4 m-auto shadow-lg rounded-xl p-4">
+      <ToastContainer />
       <h1 className="text-3xl my-4">Register</h1>
       <form onSubmit={onSubmit} className="flex flex-col space-y-4 ">
         {/* username */}
@@ -82,12 +116,50 @@ const Register = () => {
         <select
           className="border-2 border-cyan-700 p-2 rounded"
           {...register("role", { required: true })}
+          onChange={(e) => setRole(e.target.value)}
         >
           <option value="">Selecciona un rol</option>
-          <option value="doctor">Doctor</option>
           <option value="patient">Paciente</option>
+          <option value="doctor">Doctor</option>
         </select>
         {errors.role && <p>{errors.role.message}</p>}
+        {role === "doctor" && (
+          <>
+            <label htmlFor="registration_number" className="font-bold">
+              Número de Registro de colegiado
+            </label>
+            <input
+              type="text"
+              className="border-2 border-cyan-700 p-2 rounded"
+              {...register("doctor_registration_number")}
+            />
+            {errors.doctor_registration_number && (
+              <p>{errors.doctor_registration_number.message}</p>
+            )}
+            <label htmlFor="discipline_name" className="font-bold">
+              Especialidad
+            </label>
+            <select
+              className="border-2 border-cyan-700 p-2 rounded"
+              {...register("discipline_name")}
+            >
+              {discipline.map((disc, index) => (
+                <option key={index} value={disc.discipline_name}>
+                  {disc.discipline_name}
+                </option>
+              ))}
+            </select>
+            {errors.discipline_name && <p>{errors.discipline_name.message}</p>}
+            <label htmlFor="experience" className="font-bold">
+              Experiencia desde
+            </label>
+            <input
+              type="date"
+              className="border-2 border-cyan-700 p-2 rounded"
+              {...register("experience")}
+            />
+          </>
+        )}
         {/* first_name */}
         <label htmlFor="first_name" className="font-bold">
           First Name
--- Front/src/schemas/registerDoctorSchema.js
+++ Front/src/schemas/registerDoctorSchema.js
@@ -1,11 +1,10 @@
-// Importar joi
 // Importar joi y joidate y extender una encima de la otra en una constante
 import BaseJoi from "joi";
 import JoiDate from "@joi/date";
 const joi = BaseJoi.extend(JoiDate);
 import { joiErrorMessages } from "./joiErrorMessages.js";
 
-export const registerSchema = joi.object({
+export const registerDoctorSchema = joi.object({
   username: joi.string().min(3).max(30).required().messages(joiErrorMessages),
   email: joi
     .string()
@@ -37,26 +36,21 @@ export const registerSchema = joi.object({
     .max(30)
     .required()
     .messages(joiErrorMessages),
-  // doctor_registration_number: joi
-  //   .string()
-  //   .min(0)
-  //   .max(15)
-  //   .required()
-  //   .messages(joiErrorMessages)
-  //   .when("role", {
-  //     is: "doctor",
-  //     then: joi.required(),
-  //   }),
-  // discipline_name: joi.string().when("role", {
-  //   is: "doctor",
-  //   then: joi.any(),
-  // }),
-  // experience: joi.any().when("role", {
-  //   is: "doctor",
-  //   then: joi
-  //     .date()
-  //     .format("YYYY-MM-DD")
-  //     .required()
-  //     .messages({ "date.format": "La fecha debe tener el formato YYYY-MM-DD" }),
-  // }),
+  doctor_registration_number: joi
+    .string()
+    .min(0)
+    .max(15)
+    .required()
+    .messages(joiErrorMessages),
+  discipline_name: joi
+    .string()
+    .min(0)
+    .max(30)
+    .required()
+    .messages(joiErrorMessages),
+  experience: joi
+    .date()
+    .format("YYYY-MM-DD")
+    .required()
+    .messages({ "date.format": "La fecha debe tener el formato YYYY-MM-DD" }),
 });
