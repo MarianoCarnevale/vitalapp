c016a22b96151001455ebed2538ee91722931596
Adrián Acuña Cabrera <adri.acu.95@gmail.com>
2024-05-03 17:13:21 +0100

    ordenar respuestas por orden ascendente

--- Back/src/models/responses/selectAllResponsesByConsultationModel.js
+++ Back/src/models/responses/selectAllResponsesByConsultationModel.js
@@ -6,10 +6,12 @@ export const selectAllResponsesByConsultationModel = async (consultation_id) => 
     const pool = await getPool();
     // Obtener la respuesta.
     const responses = await pool.query(
-      `SELECT U.user_id, U.first_name, U.role, R.consultation_id, R.content FROM responses R
+      `SELECT U.user_id, U.first_name, U.role, R.consultation_id, R.content, R.createdAt
+        FROM responses R
         INNER JOIN users U ON R.user_id = U.user_id
         WHERE consultation_id = ?
-      GROUP BY U.user_id, R.response_id, consultation_id
+      GROUP BY U.user_id, R.response_id, consultation_id, R.createdAt
+      ORDER BY R.createdAt ASC
       `,
       [consultation_id]
     );
