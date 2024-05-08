7aad79aba3214c0f85e5f58836e36d7ccc7e24bf
Adryyy <adrydlg752@gmail.com>
2024-05-09 01:37:21 +0200

    form solo falta archivo

--- Front/src/pages/Consultation.jsx
+++ Front/src/pages/Consultation.jsx
@@ -4,28 +4,42 @@ import { useForm } from 'react-hook-form';
 import { VITE_BASE_URL } from '../config/env.js';
 
 const Consultation = () => {
-  const [disciplines, setDiscipline ] = useState([]);
-  const [disciplinesValue, setDisciplineValue] = useState([]);
+  //Almacenamos los cambios en el useState
 
-  const [doctors, setDoctors ] = useState([]);
-  const [doctor_Value, setDoctorValue] = useState([]);
+  //lista de disciplinas
+  const [disciplines, setDiscipline] = useState([]);
   
+  //Valor a enviar de disciplinas
+  const [disciplinesValue, setDisciplineValue] = useState("");
+
+  //lista de doctores
+  const [doctors, setDoctors] = useState([]);
+  
+  //valor a enviar de doctores
+  const [doctor_Value, setDoctorValue] = useState("");
+  
+  //desabilitar o habilitar select doctors y submit
   const [disable, setDisabel] = useState([true]);
 
-  //registramos todos los inputs usando register
+  //Si no hay doctores en una disciplina mandar mensaje
+  const [available, setAvailable] = useState("")
 
+  //registramos todos los inputs usando register
   const { register, handleSubmit} = useForm({
     mode: 'onTouched',
   })
 
-  //peticion de todas las disciplinas
 
+  //Despues de renderizar la pantalla
   useEffect(() => {
+
+    //Obtener todas las disciplinas
     const getDisciplines = async () => {
       try {
         const resp = await axios.get(`${VITE_BASE_URL}/disciplines`)
         const disciplines_values = Object.values(resp.data.data.disciplines) 
 
+        //Asignamos todas las disciplinas al useState
         setDiscipline(disciplines_values)
       } catch (error) {
         console.log(error);
@@ -35,45 +49,106 @@ const Consultation = () => {
   }, [])
 
 
+  //manejo de select de disciplinas
   const handelSeletDiscipline = (event) => { 
-    const discipline_value = event.target.value;
+
+    //pedir la lista de doctores
     const fetchDoctors = async () => {
+      setDisciplineValue(discipline_value)
       try {
-        const response = await axios.get(`${VITE_BASE_URL}/doctors/${disciplinesValue}`);
-        const doctors_array = Object.values(response.data.data);
-        //
+
+        const response = await axios.get(`${VITE_BASE_URL}/doctors/${discipline_value}`);
+        const doctors_array = Object.values(response.data.data);    
         setDoctors(doctors_array[0]);
-        
+        setAvailable("")
+
       } catch (error) {
+
+        //Si no hay doctor en la especilidad
+
+        //deshabilitar y restaurar los valores de doctors a null
+        setDoctors([])
+        setDoctorValue("")
+        setDisabel(true)
+
+        //mandar mensaje de disponibilidad
+        setAvailable("None doctors available")
         console.error(error);
       }
     };
     
-
-    console.log(discipline_value);
-
+    //obtener el valor de el formulario
+    const discipline_value = event.target.value;
+    
     if (discipline_value) { 
+      
+      //Si se selecciona disciplina habilitar submit
       setDisabel(false)
-      setDisciplineValue(discipline_value)
+
+      //Obtener lista de doctores
       fetchDoctors();
     } else {
+
+      //Sino deshabilitar el select de doctores
       setDisciplineValue('')
       setDiscipline([])
       setDisabel(true)
     }
   }
 
+  //Asignar valor a doctor
   const handelSeletDoctor = (event) => { 
+
     const doctor_value = event.target.value
+
     setDoctorValue(doctor_value)
   }
 
   //Pasamos OnSubmit como parametro a HandleSubmit
 
-  const OnSubmit = (data) => {
+  const OnSubmit = async (data) => {
+
+    //obtener valor de doctor
+    const doctor_value_sub = data.doctor_Value
+
+    //Si no se selecciono doctor
+    if (!doctor_value_sub) {
+
+      //Seleccionar uno random de la lista de doctores
+      const random = Math.floor(Math.random() * doctors.length)
+
+      //Asignarlo a los valores a enviar
+      data.doctor_Value = doctors[random].doctor_id
+    }
 
     //Todos los inputs del formulario
     console.log(data);
+
+    const dataToSend = {
+      doctor_id: data.doctor_Value,
+      title: data.titel,
+      description: data.description,
+      discipline_id: data.disciplinesValue,
+      severity: data.severity,
+    }
+    try {
+      const token = localStorage.getItem('token')
+
+      //Crear instancia del cuerpo form para mandar el archivo
+      const formData = new FormData
+
+      formData.append('file', data.file[0])
+
+      const resp = await axios.post(`${VITE_BASE_URL}/consultations`, dataToSend, {
+        headers: {
+          Authorization: `${token}`,
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      console.log(resp.data.message);
+    } catch (error) {
+      console.log('Error al hacer submit: ',error);
+    }
 
     
   }
@@ -86,20 +161,24 @@ const Consultation = () => {
       <form onSubmit={handleSubmit(OnSubmit)}>   
       <div>
         <h2 className="ml-6 text-sm sm:text-base sm:ml-10 p-1 mr-20 bg-white absolute text-primary rounded-sm">Speciality</h2>
-        <select className="input" value={disciplinesValue} {...register('disciplinesValue')}  onChange={handelSeletDiscipline}>
+        <select className="input" value={disciplinesValue} {...register('disciplinesValue')} onChange={handelSeletDiscipline}>
+          <option key='0' value=""></option>
           {disciplines.map(discipline => ( 
-            <option key={discipline.id} value={discipline.discipline_id}>{discipline.discipline_name}</option>
+            <option key={discipline.discipline_id} value={discipline.discipline_id}>{discipline.discipline_name}</option>
           ))}
         </select>
          <p className='text-red-500 text-sm sm:text-base pl-5'>Error Message</p>
       </div>
+
       <div>
         <h2 className="ml-6 text-sm sm:text-base sm:ml-10 p-1 mr-20 bg-white z-10 absolute text-primary rounded-sm">Doctor</h2>
-        <select className="input" value={doctor_Value} {...register('doctor_Value')} disabled={disable} onChange={handelSeletDoctor} placeholder="Doctor...">
+        <select className="input" value={doctor_Value} {...register('doctor_Value')} disabled={disable} onChange={handelSeletDoctor}>
+          <option key='1' value="">None selected</option>
          {doctors.map(doctor => ( 
-            <option key={doctor.id} value={doctor.doctor_registration_number}>Doctor: {doctor.first_name}</option>
+            <option key={doctor.doctor_id} value={doctor.doctor_id}>{doctor.first_name}</option>
           ))}
         </select>
+
          <p className='text-red-500 text-sm sm:text-base pl-5'>Error Message</p>
       </div>
       <div>
@@ -144,7 +223,8 @@ const Consultation = () => {
       </label>
 
       </div>
-      <button className='bg-primary p-2 m-3 mt-5 w-11/12 rounded-md text-white active:bg-white active:text-black border-2 border-primary'>Submit</button>
+      <button className='bg-primary p-2 m-3 mt-5 w-11/12 rounded-md text-white active:bg-white active:text-black border-2 border-primary disabled:bg-gray-500' disabled={disable}>Submit</button>
+      <p className='text-red-500 text-sm sm:text-base pl-5'>{available}</p>
     </form>
 
   </div>;
