ea4c8e02567973a18663fe4469e2a914568922da
Martín Castro <castro.cores77@gmail.com>
2024-05-09 14:13:25 +0200

    cambio en schemaconsultations en el back

--- Back/src/schemas/consultations/consultationsFilterSchema.js
+++ Back/src/schemas/consultations/consultationsFilterSchema.js
@@ -3,12 +3,24 @@ import joi from 'joi';
 import { joiErrorMessagesConsultations } from './joiErrorMessagesConsultations.js';
 
 export const newConsultationsSchema = joi.object({
-  user_id: joi.number().required(),
-  doctor_id: joi.number().required(),
-  title: joi.string().min(3).max(30).required().messages(joiErrorMessagesConsultations),
-  description: joi.string().min(4).required().messages(joiErrorMessagesConsultations),
-  file: joi.string().min(4).messages
-  (joiErrorMessagesConsultations),
+  user_id: joi.string().required(),
+  doctor_id: joi.string().required(),
+  title: joi
+    .string()
+    .min(3)
+    .max(30)
+    .required()
+    .messages(joiErrorMessagesConsultations),
+  description: joi
+    .string()
+    .min(4)
+    .required()
+    .messages(joiErrorMessagesConsultations),
+  file: joi.string().min(4).messages(joiErrorMessagesConsultations),
   discipline_id: joi.number().required(),
-  severity: joi.string().valid('high', 'medium','low').required().messages(joiErrorMessagesConsultations),
+  severity: joi
+    .string()
+    .valid('high', 'medium', 'low')
+    .required()
+    .messages(joiErrorMessagesConsultations),
 });
