87413bb1593bd79f923907513c2d496511914586
Mariano Carnevale <marianocarne900@gmail.com>
2024-05-16 19:07:05 +0200

    mui, darkMode

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,18 +2,22 @@ import "./index.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-
+import { ThemeProvider } from "@mui/material/styles";
 import { UserTokenProvider } from "./contexts/UserTokenContext.jsx";
+import { DarkModeProvider } from "./contexts/DarkModeContext.jsx";
+import theme from "./theme";
 
 function App() {
   return (
-    <>
-      <UserTokenProvider>
-        <Header />
-        <Main />
-        <Footer />
-      </UserTokenProvider>
-    </>
+    <UserTokenProvider>
+      <ThemeProvider theme={theme}>
+        <DarkModeProvider>
+          <Header />
+          <Main />
+          <Footer />
+        </DarkModeProvider>
+      </ThemeProvider>
+    </UserTokenProvider>
   );
 }
 
