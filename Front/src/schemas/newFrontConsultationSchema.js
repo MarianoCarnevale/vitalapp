e74a57e913182be836270c37a1b5ac7b4708593f
Adrián Acuña Cabrera <adri.acu.95@gmail.com>
2024-05-27 13:26:55 +0100

    quitar limitacion descripcion

--- Front/src/schemas/newFrontConsultationSchema.js
+++ Front/src/schemas/newFrontConsultationSchema.js
@@ -4,7 +4,7 @@ import { joiErrorMessages } from '../../../Front/src/schemas/joiErrorMessages.js
 export const newFrontConsultationsSchema = joi.object({
   doctor: joi.allow().messages(joiErrorMessages),
   titulo: joi.string().min(3).max(20).required().messages(joiErrorMessages),
-  descripcion: joi.string().min(3).max(100).required().messages(joiErrorMessages),
+  descripcion: joi.string().min(3).max(500).required().messages(joiErrorMessages),
   especialidad: joi.required().messages(joiErrorMessages),
   gravedad: joi.string().required().messages(joiErrorMessages),
   file: joi.allow().messages(joiErrorMessages),
