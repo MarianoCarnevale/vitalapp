8e166ac084382548f9dcdc49d8505ad63e60cb7c
Mariano Carnevale <marianocarne900@gmail.com>
2024-05-29 12:32:55 +0200

    cambios deploy

--- Back/src/db/getPool.js
+++ Back/src/db/getPool.js
@@ -1,39 +1,20 @@
-// Importar la versión asincrona de mysql2
 import mysql from 'mysql2/promise';
-
-// Obtener las variables de entorno
-import {
-  MYSQL_HOST,
-  MYSQL_USER,
-  MYSQL_PASSWORD,
-  MYSQL_DATABASE,
-} from '../../env.js';
 import { generateError } from '../utils/errors/generateError.js';
+import url from 'url';
 
-// Crear una variable para almacenar el pool de conexiones
 let pool;
 
-// Crear una función para crear y devolver el pool de conexiones
 export const getPool = async () => {
   try {
     if (!pool) {
-      // Crear un pool temporal para conectarse al servidor de MySQL
-      const poolTemp = mysql.createPool({
-        host: MYSQL_HOST,
-        user: MYSQL_USER,
-        password: MYSQL_PASSWORD,
-      });
-
-      // Crear la base de datos si no existe
-      await poolTemp.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}`);
-
-      // Creamos un nuevo pool de conexiones a la base de datos
+      const dbUrl = new url.URL(process.env.DATABASE_URL);
       pool = mysql.createPool({
+        host: dbUrl.hostname,
+        user: dbUrl.username,
+        password: dbUrl.password,
+        database: dbUrl.pathname.substr(1),
+        port: dbUrl.port,
         connectionLimit: 10,
-        host: MYSQL_HOST,
-        user: MYSQL_USER,
-        password: MYSQL_PASSWORD,
-        database: MYSQL_DATABASE,
         timezone: 'Z',
       });
     }
